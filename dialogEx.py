101759616b13db7ca167f0b0e23265e8b5f4e81f
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-01-26 02:40:05 -0300

    Update dialogEx
    Mudando Layout

--- dialogEx.py
+++ dialogEx.py
@@ -5,43 +5,53 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 
 import main
 
 kivy.require('2.1.0') # replace with your current kivy version !
 
-class MainGrid(GridLayout):
+class Grid(GridLayout):
     #Initialize infinite keywords: **kwargs
     def __init__(self, **kwargs):
         #Calling the grid constructor
-        super(MainGrid, self).__init__(**kwargs)
+        super(Grid, self).__init__(**kwargs)
         #Setting collums
-        self.cols = 4
+        self.cols = 1
+        #Creating Grids
+        self.headerGrid = GridLayout()
+        self.headerGrid.cols = 4
+        self.mainGrid = GridLayout()
+        self.mainGrid.cols = 4
+        self.footerGrid = GridLayout()
+        self.footerGrid.cols = 3
         #Adding widgets
+        #header
+        self.mainGrid.add_widget(Label(text="ID"))
+        self.mainGrid.add_widget(Label(text="Sobrenome"))
+        self.mainGrid.add_widget(Label(text="Nome"))
+        self.mainGrid.add_widget(Label(text="Presença"))
+        #main
         main.sqlOpen()
         global member 
         member = main.listAllMembers()
         c = 0
-        
         while c < len(member):
-            #Button
-            self.id = Button(text=f"{member[c][0]}")
-            self.id.bind(on_press=self.press)
-            self.add_widget(self.id)
-            #self.add_widget(Label(text=f"{member[c][1]}"))
-            self.lastName = TextInput(text=f"{member[c][1]}", multiline=False)
-            self.add_widget(self.lastName)
-            self.name = TextInput(text=f"{member[c][2]}", multiline=False)
-            self.add_widget(self.name)
-            self.add_widget(Label(text=f"{member[c][3]}"))
+            self.mainGrid.add_widget(Label(text=f"{member[c][0]}"))
+            self.mainGrid.add_widget(Label(text=f"{member[c][1]}"))
+            self.mainGrid.add_widget(Label(text=f"{member[c][2]}"))
+            self.mainGrid.add_widget(Label(text=f"{member[c][3]}"))
             c += 1
-    def press(self,instance):
-        id = instance.text
-        print(f"rowId = {id}")
+        #footer
+        self.footerGrid.add_widget(Button(text="Voltar"))
+        self.footerGrid.add_widget(Button(text="Confirmar"))
+        #self.add_widget(self.headerGrid)
+        self.add_widget(self.mainGrid)
+        self.add_widget(self.footerGrid)
         
 class SacramentalAttendance(App):
         def build(self):
-            return MainGrid()
+            return Grid()
 
     
     
